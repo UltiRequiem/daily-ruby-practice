0ef65421fd0e464aefcfb08f1f5aaed1efdcd246
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-23 21:42:02 -0500

    :art: Day 3

---
+++ geeks_for_geeks/lists/03_find_length_of_list.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# https://www.geeksforgeeks.org/python-ways-to-find-length-of-list
+
+# Find length of a list.
+
+NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8].freeze
+
+puts("The leng of the list is #{NUMBERS.length}.")
