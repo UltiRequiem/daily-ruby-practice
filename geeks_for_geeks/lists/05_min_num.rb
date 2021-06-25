fab6bf96c47d48ffb17379193c3fc29eca890f1e
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-25 12:07:33 -0500

    :art: Day 05

---
+++ geeks_for_geeks/lists/05_min_num.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# https://www.geeksforgeeks.org/minimum-of-two-numbers-in-python
+
+# Given a list, write a Python code to find the Minimum of these two numbers.
+
+NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8].freeze
+
+puts NUMBERS.min
