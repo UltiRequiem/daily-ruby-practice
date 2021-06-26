dcad39055b27a1d6b881db9a414730cb82332167
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-26 14:47:12 -0500

    :art: Update

--- geeks_for_geeks/lists/01_interchange_first_last_element.rb
+++ geeks_for_geeks/lists/01_interchange_first_last_element.rb
@@ -4,8 +4,8 @@ my_list = [20, 50, 60, 30]
 
 def swap_list(list_to_swap)
   list_to_swap[0], list_to_swap[-1] = list_to_swap[-1], list_to_swap[0]
-
-  list_to_swap
 end
 
-puts swap_list(my_list)
+swap_list(my_list)
+
+puts my_list
