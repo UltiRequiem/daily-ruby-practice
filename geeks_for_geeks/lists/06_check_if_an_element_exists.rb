0d1ad740264ec9a3e999f7571109994d81c9e422
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-13 15:05:47 -0500

    :art: RuboCop

--- geeks_for_geeks/lists/06_check_if_an_element_exists.rb
+++ geeks_for_geeks/lists/06_check_if_an_element_exists.rb
@@ -3,4 +3,4 @@
 MY_LIST = [1, 6, 3, 5, 3, 4].freeze
 ELE_TO_FIND = 5
 
-puts "#{ELE_TO_FIND} exists in the the test list." if MY_LIST.include? ELE_TO_FIND
+puts "#{ELE_TO_FIND} exists in the list." if MY_LIST.include? ELE_TO_FIND
