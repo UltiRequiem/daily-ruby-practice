1efb9a5f15d904e17173e100e87d49adb5cd3a9a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-16 09:28:20 -0500

    :sparkles: Day 25

---
+++ geeks_for_geeks/lists/25_remove_empty.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+EXAMPLE = [[],
+           %w[ram 15 8],
+           [],
+           %w[laxman sita],
+           %w[krishna akbar 45],
+           ['', ''],
+           []].freeze
+
+def remove_empty_lists(tuples)
+  tuples.reject(&:empty?)
+end
+
+print(remove_empty_lists(EXAMPLE))
