48ebcb895717cfab24f65ea4cd7715bf59b46f75
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-19 10:12:28 -0500

    :sparkles: Day 28

--- geeks_for_geeks/lists/07_clear.rb
+++ geeks_for_geeks/lists/07_clear.rb
@@ -2,6 +2,13 @@
 
 # https://www.geeksforgeeks.org/different-ways-to-clear-a-list-in-python
 
-MY_LIST = [6, 0, 4, 1].clear
+MY_LIST = [6, 0, 4, 1].freeze
+
+def clean_list(lst)
+  new_list = lst.map(&:clone)
+  new_list.clear
+  new_list
+end
 
 puts "My List contain #{MY_LIST}."
+puts "My cleaned list contain #{clean_list(MY_LIST)}."
