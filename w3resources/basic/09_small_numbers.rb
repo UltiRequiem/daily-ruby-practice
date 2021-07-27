18d2a74329a82977c6bfc11bebc83e9aca715766
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-27 17:08:55 -0500

    :art: Day 36
    En casa de los abuelos :)

--- w3resources/basic/09_small_numbers.rb
+++ w3resources/basic/09_small_numbers.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-def small?(first, second, third)
-  ((first >= 1 && first <= 10) || (second >= 1 && second <= 10) || (third >= 1 && third <= 10))
+def small?(numbers)
+  numbers.any? { |num| num.between?(1, 10) }
 end
 
-puts small?(1, 12, 25)
-puts small?(10, 15, 33)
-puts small?(11, 25, 43)
+puts small?([1, 12, 25])
+puts small?([10, 15, 33])
+puts small?([11, 25, 43])
