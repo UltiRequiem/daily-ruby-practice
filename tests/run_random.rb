a456a440ea6728a4c06e5e5993af2c3af669760c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-18 12:00:23 -0500

    :sparkles: Update

--- tests/run_random.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-CURRENT_PATH = `pwd`.strip
-DIRECTORY = "#{CURRENT_PATH}/geeks_for_geeks/lists/"
-SCRIPT = `ls #{DIRECTORY}*.rb`.split.sample
-
-puts "The Script to run is #{SCRIPT}"
-system("ruby #{SCRIPT}")
